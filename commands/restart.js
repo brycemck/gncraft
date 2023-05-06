fdea2d039ae26702973ab38ca6326ab48279dc46
Bryce McKenney <brycemckenney@gmail.com>
2023-05-06 11:32:33 -0700

    added restart command

---
+++ commands/restart.js
@@ -0,0 +1,42 @@
+const { exec } = require('child_process');
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+  name: 'restart',
+  description: '',
+  usage: '',
+  embeddedMessage: {
+    color: 0x0099FF,
+    title: 'GaynadianCraft',
+    fields: [],
+    timestamp: new Date().toISOString()
+  },
+  process: () => {
+    return new Promise((resolve, reject) => {
+      exec('sh /opt/restart-server.sh 9', (err, stdout, stderr) => {
+        if (err) {
+          reject(err)
+        } else {
+          console.log(stderr)
+          resolve(stdout)
+        }
+      })
+    })
+  },
+  SlashCommand: {
+    options: [],
+    run: async (client, interaction) => {
+      const that = module.exports;
+
+      that.embeddedMessage.fields = [];
+      that.process().then((response) => {
+        that.embeddedMessage.fields.push({name: 'Restarting Minecraft service...', value: `\`${response}\``})
+        return interaction.reply({embeds: [that.embeddedMessage]})
+      }).catch((err) => {
+        that.embeddedMessage.fields.push({name: 'Error running restart', value: `\`${err}\``})
+        return interaction.reply({embeds: [that.embeddedMessage]})
+      })
+    }
+  }
+}
